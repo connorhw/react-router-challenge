2da7e33fc94f2d9efde0ea1d119d2966171bde63
Connor Wong <connorwong93@gmail.com>
2020-02-17 18:12:04 -0800

    Route is wired correctly

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import AboutMenu from './about-menu';
+import { Route } from 'react-router-dom'
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            {/*<AboutMenu />*/}
+            <Route 
+                exact path='/about/foo'
+                component={AboutMenu}
+            />
         </div>
     );
 }
